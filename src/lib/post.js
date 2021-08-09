addb3908d4153a494b1d80cfdb2b92df6b086870
jiyoon0605 <ljy014@naver.com>
2021-08-09 13:46:09 +0900

    deploy test

--- src/lib/post.js
+++ src/lib/post.js
@@ -1,36 +1,36 @@
 import { auth } from "./auth";
 
 export async function getAllPosts() {
-  // const response = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/posts`
-  // );
-  // const { posts } = await response.json();
-
-  // const sortable = [];
-  // for (let post of posts) {
-  //   sortable.push(post);
-  // }
-  // sortable.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  const res = await fetch(
-    `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Posts`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-      },
-    }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/posts`
   );
-  const { records } = await res.json();
-  const posts = records.map((record) => ({
-    id: record.id,
-    ...record.fields,
-  }));
+  const { posts } = await response.json();
 
   const sortable = [];
   for (let post of posts) {
     sortable.push(post);
   }
   sortable.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  // const res = await fetch(
+  //   `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Posts`,
+  //   {
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+  //     },
+  //   }
+  // );
+  // const { records } = await res.json();
+  // const posts = records.map((record) => ({
+  //   id: record.id,
+  //   ...record.fields,
+  // }));
+
+  // const sortable = [];
+  // for (let post of posts) {
+  //   sortable.push(post);
+  // }
+  // sortable.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return sortable;
 }
