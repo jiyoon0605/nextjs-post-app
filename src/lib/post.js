84a6a0166fb640ab83b5aaa5f22abf9e88f883ec
jiyoon0605 <ljy014@naver.com>
2021-08-09 13:32:21 +0900

    deploy test

--- src/lib/post.js
+++ src/lib/post.js
@@ -1,18 +1,28 @@
 import { auth } from "./auth";
 
 export async function getAllPosts() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/posts`
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/posts`
+  // );
+  // const { posts } = await response.json();
+
+  // const sortable = [];
+  // for (let post of posts) {
+  //   sortable.push(post);
+  // }
+  // sortable.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  const res = await fetch(
+    `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Posts`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+      },
+    }
   );
-  const { posts } = await response.json();
-
-  const sortable = [];
-  for (let post of posts) {
-    sortable.push(post);
-  }
-  sortable.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  return sortable;
+  const { records } = await res.json();
+  console.log(records);
+  return []; //sortable;
 }
 
 export async function createPost(data) {
